c1dda79ec78ea8f270f9fc8d545f61df29f2719d
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-06-14 16:53:56 +0530

    update backend gitignore

--- backend/dist/modules/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appRouter = void 0;
-const express_1 = require("express");
-const todo_router_1 = require("../todo/todo.router");
-const appRouter = (0, express_1.Router)();
-exports.appRouter = appRouter;
-appRouter.use('/todos', todo_router_1.todoRouter);
